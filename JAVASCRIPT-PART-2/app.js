6cd0cfebb964f655638e57cd733f747a0bb99d70
soyambuilds <soyam.builds@gmail.com>
2026-08-10 21:54:09 +0530

    Adding JS-PART-2

---
+++ JAVASCRIPT-PART-2/app.js
@@ -0,0 +1,80 @@
+// console.log("Hello World");
+// console.log("Soyam Builds");
+
+// let a = 10;
+// let b = 5;
+
+// console.log("Sum is :", a + b);
+
+// let pencilPrice = 10;
+// let eraserPrice = 5;
+
+// let output = "The total price is :" + (pencilPrice + eraserPrice) + " Rupees."
+// let output = `The total price is : ${pencilPrice + eraserPrice} Rupees.`;
+
+// console.log(`The total price is : ${pencilPrice + eraserPrice} Rupees.`);
+
+//Arithmetic Operators
+// let a = 10;
+// let b = 5;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// console.log(a ** b);
+
+// console.log(a++);
+// console.log(++a);
+
+// b = a;
+// console.log(b);
+
+//Operators
+// let age = 23;
+
+// console.log(age > 18);
+// console.log(age >= 18);
+// console.log(age < 18);
+// console.log(age <= 18);
+
+//Conditional Statements
+// console.log("Before my if statement");
+// let age = 33;
+// if (age >= 18) {
+//   console.log("You Can Vote");
+//   console.log("You Can Drive");
+
+//   let a = 5;
+//   console.log(5 * 2);
+// }
+
+// if (age < 18) {
+//     console.log("You can not vote");
+// }
+
+// if (age >= 20) {
+//     console.log("You are in your 20s");
+// }
+// console.log("After my if statement");
+
+// let firstName = "soyam";
+// if (firstName == "soyam") {
+//     console.log(`Welcome ${firstName}`);
+// }
+
+//Traffic Light System
+
+let color = "green";
+
+if(color === "red") {
+    console.log("STOP ! Light Color is RED");
+}
+if(color === "yellow") {
+    console.log("START ! Light Color is YELLOW");
+}
+if(color === "green") {
+    console.log("GO ! Light Color is GREEN");
+}
+
