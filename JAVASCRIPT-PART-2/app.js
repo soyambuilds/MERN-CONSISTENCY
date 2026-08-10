13df2c14d01878483479fe9dbf0fb265713e88c5
soyambuilds <soyam.builds@gmail.com>
2026-08-11 00:48:27 +0530

    Updated

--- JAVASCRIPT-PART-2/app.js
+++ JAVASCRIPT-PART-2/app.js
@@ -66,15 +66,84 @@
 
 //Traffic Light System
 
-let color = "green";
+// let color = "green";
 
-if(color === "red") {
-    console.log("STOP ! Light Color is RED");
-}
-if(color === "yellow") {
-    console.log("START ! Light Color is YELLOW");
-}
-if(color === "green") {
-    console.log("GO ! Light Color is GREEN");
-}
+// if(color === "red") {
+//     console.log("STOP ! Light Color is RED");
+// }
+// if(color === "yellow") {
+//     console.log("START ! Light Color is YELLOW");
+// }
+// if(color === "green") {
+//     console.log("GO ! Light Color is GREEN");
+// }
 
+// let age = 144;
+
+// if(age >= 18) {
+//     console.log("You can vote");
+// }
+// else if(age >= 18) {
+//     console.log("You can not vote");
+// }
+
+// let marks = 15;
+
+// if (marks >= 80) {
+//     console.log("You are A+");
+// }
+// else if (marks >= 60) {
+//     console.log("You are A");
+// }
+// else if (marks >= 33) {
+//     console.log("You are B");
+// }
+// else if (marks < 33) {
+//     console.log("You are Fail");
+// }
+
+// let month = "may";
+
+// if(month === "january") {
+//     console.log("Winter Here");
+// }
+// else if(month === "may") {
+//     console.log("summer Here");
+// }
+
+// let age = 13;
+// if (age >= 18) {
+//   console.log("You can vote");
+// } else {
+//   console.log("You can not vote");
+// }
+
+// let color = "pink";
+
+// if (color === "red") {
+//   console.log("STOP");
+// } else if (color === "yellow") {
+//   console.log("START");
+// } else if (color === "green") {
+//   console.log("GOO");
+// } else {
+//   console.log("PICK RIGHT COLOR");
+// }
+
+let size = "N";
+
+if (size === "XL") {
+    console.log("Price is Rs. 250/-");
+}
+else if (size === "L") {
+    console.log("Price is Rs. 200/-");
+}
+else if (size === "M") {
+    console.log("Price is Rs. 100/-");
+}
+else if (size === "S") {
+    console.log("Price is Rs. 50/-");
+}
+else {
+    console.log("Choose a valid Size");
+}
