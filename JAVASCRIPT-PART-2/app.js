273624a0f09ea9694cfb5cc7aed353d64164c234
soyambuilds <soyam.builds@gmail.com>
2026-08-11 21:41:40 +0530

    Updated JS_PART_2

--- JAVASCRIPT-PART-2/app.js
+++ JAVASCRIPT-PART-2/app.js
@@ -130,20 +130,218 @@
 //   console.log("PICK RIGHT COLOR");
 // }
 
-let size = "N";
+// let size = "N";
 
-if (size === "XL") {
-    console.log("Price is Rs. 250/-");
-}
-else if (size === "L") {
-    console.log("Price is Rs. 200/-");
-}
-else if (size === "M") {
-    console.log("Price is Rs. 100/-");
-}
-else if (size === "S") {
-    console.log("Price is Rs. 50/-");
-}
-else {
-    console.log("Choose a valid Size");
-}
+// if (size === "XL") {
+//     console.log("Price is Rs. 250/-");
+// }
+// else if (size === "L") {
+//     console.log("Price is Rs. 200/-");
+// }
+// else if (size === "M") {
+//     console.log("Price is Rs. 100/-");
+// }
+// else if (size === "S") {
+//     console.log("Price is Rs. 50/-");
+// }
+// else {
+//     console.log("Choose a valid Size");
+// }
+
+// let marks = 13;
+
+// if (marks >= 33) {
+//     if (marks >= 80) {
+//         console.log("A+");
+//     }
+//     else {
+//         console.log("A");
+//     }
+// }
+// else {
+//     console.log("Better Luck Next Time");
+// }
+
+// Logical Operators
+
+// let marks = 40;
+
+// if (marks >= 33 && marks >=80) {
+//     console.log("pass");
+//     console.log("A+");
+// }
+
+// let marks = 40;
+
+// if (marks >= 33 || marks >=80) {
+//     console.log("pass");
+//     console.log("A+");
+// }
+
+// let marks = 99;
+
+// if (!(marks < 33)) {
+//     console.log("pass");
+//     console.log("A+");
+// }
+
+// if ((marks > 33 && marks <= 80) || !false) {
+//     console.log("pass");
+// }
+
+// Practice Qs
+
+// let str = "anuvv";
+
+// if ((str[0] === "a") && (str.length > 3)) {
+//     console.log("It is a good string");
+// }
+// else {
+//     console.log("It is not a good string");
+// }
+
+// let num = 12;
+
+// if((num % 3 === 0) && ( (num + 1 === 15) || (num - 1 === 11))) {
+//     console.log("safe");
+// }
+// else {
+//     console.log("unsafe");
+// }
+
+// if (true) {
+//     console.log("It has true value");
+// }
+// else {
+//     console.log("It has false value");
+// }
+
+// if (false) {
+//     console.log("It has true value");
+// }
+// else {
+//     console.log("It has false value");
+// }
+
+// if (0) {
+//     console.log("It has true value");
+// }
+// else {
+//     console.log("It has false value");
+// }
+
+// if ("apna college") {
+//     console.log("It has true value");
+// }
+// else {
+//     console.log("It has false value");
+// }
+
+// if (null) {
+//     console.log("It has true value");
+// }
+// else {
+//     console.log("It has false value");
+// }
+
+// if (undefined) {
+//     console.log("It has true value");
+// }
+// else {
+//     console.log("It has false value");
+// }
+
+// if ("") {
+//     console.log("It has true value");
+// }
+// else {
+//     console.log("It has false value");
+// }
+
+// if (" ") {
+//     console.log("It has true value");
+// }
+// else {
+//     console.log("It has false value");
+// }
+
+// let str = "";
+
+// if(str) {
+//     console.log("String is not empty");
+// }
+// else {
+//     console.log("String is empty");
+// }
+
+// let num = 0;
+
+// if (num) {
+//     console.log("Num is not Zero");
+// }
+// else {
+//     console.log("Num is Zero")
+// }
+
+// let color = "green";
+
+// switch (color) {
+//   case "red":
+//     console.log("Stop");
+//     break;
+//   case "yellow":
+//     console.log("Start");
+//     break;
+//   case "green":
+//     console.log("Goo");
+//     break;
+//   default:
+//     console.log("Broken Light");
+// }
+
+// console.log("After Switch Statement");
+
+// let day = 7;
+
+// switch(day) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thursday");
+//         break;
+//     case 5:
+//         console.log("Friday");
+//         break;
+//     case 6:
+//         console.log("Saturday");
+//         break;
+//     case 7:
+//         console.log("Sunday");
+//         break;
+//     default:
+//         console.log("Choose a valid day between 1 to 7");
+// }
+
+// alert("DANGER");
+
+// console.log("This is a Simple Log");
+// console.error("This is an Error message");
+// console.warn("This is an Warning message");
+
+// let firstName = prompt("Enter Your Name :");
+// console.log(firstName);
+
+// let rollNumber = prompt("Enter Your Roll-no :");
+// console.log(rollNumber);
+
+let firstName = prompt("Enter Your first name : ");
+let lastName = prompt("Enter Your last name : ");
+
+alert(`The Name is ${firstName + " " + lastName}`);
