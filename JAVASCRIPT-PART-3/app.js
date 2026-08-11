d948afffeb936bc30c6732a5fb3c4ad9212cf87b
soyambuilds <soyam.builds@gmail.com>
2026-08-12 01:33:41 +0530

    Adding JS-PART-3

---
+++ JAVASCRIPT-PART-3/app.js
@@ -0,0 +1,44 @@
+// let str = "   He  llo  ";
+
+// let password = prompt("Set Your Password :");
+// let newPass = password.trim();
+// console.log(password);
+
+// let name = "apna college";
+
+// let msg = "Error";
+// console.log(msg.toUpperCase());
+// console.log(msg.toLowerCase());
+
+// let msg = "IloveCoding";
+
+// let msg = "  hello  ";
+// let newMsg = msg.trim();
+// console.log("after trim :", newMsg);
+// newMsg = newMsg.toUpperCase();
+// console.log("after uppercase :", newMsg);
+
+// let newMsg = msg.trim().toUpperCase();
+// console.log(newMsg);
+
+// let msg = "hello";
+// console.log(msg.slice(0,4));
+
+// let msg = "apnacollege";
+// console.log(msg.slice(-6));
+
+// let msg = "ILoveCoding";
+
+// let fruit = "Mango";
+
+// let msg = "help!";
+// console.log(msg.trim().toUpperCase());
+
+// let namee = "ApnaCollege";
+// console.log(namee.slice(4,9));
+// console.log(namee.indexOf("na"));
+// console.log(namee.replace("Apna","Our"));
+
+// let newName = namee.slice(4);
+// console.log(newName.replace("l","t"));
+
---
+++ JAVASCRIPT-PART-3/app2.js
@@ -0,0 +1,21 @@
+// let student1 = "aman";
+// let student2 = "shradha";
+// let student3 = "soyam";
+
+// let students = ["aman","shradha","rajat"];
+
+// let cars = ["audi", "bmw", "mercedes", "tata"];
+
+// let months = ["january","july","march","august"];
+
+// months[1] = "june";
+// months[0] = "july";
+
+// months.shift();
+// months.shift();
+// months.unshift("june");
+// months.unshift("july");
+
+// console.log(months);
+
+let cars = ["tata","bmw","toyota","hyundai"];
