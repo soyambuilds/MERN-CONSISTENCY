09be117002b867b7715ddba280f457fea564a771
soyambuilds <soyam.builds@gmail.com>
2026-08-20 16:08:32 +0530

    JS_PART_8 COMPLETED

--- JAVASCRIPT-PART-8/app.js
+++ JAVASCRIPT-PART-8/app.js
@@ -120,3 +120,81 @@
 // console.log(sum(1, 3));
 
 // console.log(sum(3)); a = 3 and b = undefined .
+
+// let arr = [1, 2, 3, 12, 35];
+// let newArr = [...arr];
+
+// let chars = [..."Soyam"];
+
+// let odd = [1, 3, 5, 7, 9];
+// let even = [2, 4, 6, 8, 10];
+
+// let nums = [...odd, ...even];
+// let nums2 = [...even, ...odd];
+
+// const data = {
+//   emali: "soyam@gmail.com",
+//   password: "abcd",
+// };
+
+// const dataCopy = {...data, id: 123, country: "india"};
+
+// let arr = [1, 2, 3, 4, 5]; //val
+// let obj1 = {...arr}; // key -> val
+
+// let obj = {..."Soyam"};
+
+// function sum(...args) {
+//   for (let i = 0; i < args.length; i++) {
+//     console.log("You gave us: ", args[i]);
+//   }
+// }
+
+// function min(a, b, c, d) {
+//   console.log(arguments);
+// }
+
+// function min() {
+//   console.log(arguments);
+//   console.log(arguments.length);
+//   arguments.push(1);
+// }
+
+// function sum(...args) {
+//   return args.reduce((sum, el) => sum + el);
+// }
+
+// function min(...args) {
+//   return args.reduce((min, el) => {
+//     if (min > el) {
+//       return el;
+//     } else {
+//       return min;
+//     }
+//   });
+// }
+
+// let names = ["tony", "spider", "hulk", "shadow", "abc", "xyz", "pyq"];
+// // let winner = names[0];
+// // let runnerup = names[1];
+// // let secondRunnerup = names[2];
+
+// // let [winner, runnerup, secondRunnerup] = names;
+
+// // let [winner, runnerup, others] = names;
+// let [winner, runnerup, ...others] = names;
+
+const student = {
+  name: "karan",
+  age: 14,
+  class: 9,
+  subjects: ["hindi", "english", "math", "science"],
+  username: "karan@123",
+  password: "abcd",
+//   city: "Pune",
+};
+
+// let username = student.username;
+// let password = student.password;
+
+let {username: user, password: pass, city: loc = "Mumbai"} = student;
