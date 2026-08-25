a41b14fad0f1eb3408d024c927bd500caa89c344
soyambuilds <soyam.builds@gmail.com>
2026-08-26 02:37:30 +0530

    COMPLETED JS_PART_12

---
+++ JAVASCRIPT-PART-12/api.js
@@ -0,0 +1,36 @@
+let url = "https://catfact.ninja/fact";
+
+// fetch(url)
+//   .then((res) => {
+//     return res.json();
+//   })
+//   .then((data) => {
+//     console.log("data1 : ", data.fact);
+//     return fetch(url);
+//   })
+//   .then((res) => {
+//     return res.json();
+//   })
+//   .then((data2) => {
+//     console.log("data2: ", data2.fact);
+//   })
+//   .catch((err) => {
+//     console.log("ERROR - ", err);
+//   });
+
+//   console.log("I am Happy");
+
+async function getFacts() {
+  try {
+    let res = await fetch(url);
+    let data = await res.json();
+    console.log(data.fact);
+
+    let res2 = await fetch(url);
+    let data2 = await res2.json();
+    console.log(data2.fact);
+  } catch (err) {
+    console.log("error - ", err);
+  }
+  console.log("bye");
+}
